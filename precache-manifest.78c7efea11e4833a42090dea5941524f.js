3969af2901092f062c1a400989bd35f0d806ab60
lakeMDJ <liangran@DESKTOP-GHUBIEU.localdomain>
2019-12-28 16:58:50 -0800

    Updates

---
+++ precache-manifest.78c7efea11e4833a42090dea5941524f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447e1f91ba50fc2806502f37d8cfa008",
+    "url": "/price-calculator/index.html"
+  },
+  {
+    "revision": "6f9a4f709df5a5ac5543",
+    "url": "/price-calculator/static/css/main.91b6221a.chunk.css"
+  },
+  {
+    "revision": "3a0049001ee3ef647514",
+    "url": "/price-calculator/static/js/2.c308eee2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/price-calculator/static/js/2.c308eee2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f9a4f709df5a5ac5543",
+    "url": "/price-calculator/static/js/main.2af03648.chunk.js"
+  },
+  {
+    "revision": "9144943f279b2d75963b",
+    "url": "/price-calculator/static/js/runtime-main.99758b03.js"
+  }
+]);
